96dc3ca79678bff58cfe92aed1fc1cc289bd87a9
3k-dome <doenerfresser3k@gmail.com>
2023-10-05 10:52:47 +0200

    engine changes & updates

--- catan_engine/parameters.py
+++ catan_engine/parameters.py
@@ -10,9 +10,13 @@ class EngineParameters:
     episodes: int
     port: int
     seed: int
+    rolls: int = 100
+    stats: bool = False
+    prefix: str = ""
 
     def as_args(self) -> str:
         return (
             f"{'--single' if self.single else ''} {'--verbose' if self.verbose else ''} {'--assisted' if self.assisted else ''} "
-            + f"--episodes {self.episodes} --port {self.port} --seed {self.seed} {'--init' if self.init else ''}"
+            + f"--episodes {self.episodes} --port {self.port} --seed {self.seed} {'--init' if self.init else ''} --rolls {self.rolls} "
+            + f"{'--stats' if self.stats else ''} {'--prefix' if self.stats else ''} {self.prefix if self.stats else ''}"
         )
